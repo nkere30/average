97352b2c11dbe8e2ee0352a4903cd52c9f4222c6
Nino Kereselidze <ninokereselidze30@gmail.com>
2023-08-28 18:59:45 +0400

    Practice : Average Commit

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Use Scanner methods to read input
+        // Calculates and prints average of user enetered numbers
+        int average = calculateAverage(scanner);
+        System.out.println("The average is: " + average);
 
     }
 
+    /*
+    Calculates average of user enetered numbers
+     */
+    static int calculateAverage(Scanner scanner) {
+        int sum = 0;
+        int numberCount = 0;
+        while(true) {
+            int number = scanner.nextInt();
+            if(number == 0) break;
+            sum +=number;
+            numberCount++;
+        }
+        return sum/numberCount;
+    }
 }
